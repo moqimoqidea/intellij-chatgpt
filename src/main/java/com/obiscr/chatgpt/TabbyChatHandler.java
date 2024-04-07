0029f13c9fc42427cc8d6f0f677a3abecf93972f
moqimoqidea <moqimoqidea@gmail.com>
2024-04-07 14:43:33 +0800

    Use env: TABBY_CHAT_URL

--- src/main/java/com/obiscr/chatgpt/TabbyChatHandler.java
+++ src/main/java/com/obiscr/chatgpt/TabbyChatHandler.java
@@ -1,9 +1,7 @@
 package com.obiscr.chatgpt;
 
 import com.intellij.openapi.project.Project;
-import com.obiscr.chatgpt.core.builder.OfficialBuilder;
 import com.obiscr.chatgpt.core.parser.OfficialParser;
-import com.obiscr.chatgpt.settings.OpenAISettingsState;
 import com.obiscr.chatgpt.ui.MainPanel;
 import com.obiscr.chatgpt.ui.MessageComponent;
 import com.obiscr.chatgpt.util.StringUtil;
@@ -17,12 +15,8 @@ import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.net.ssl.X509TrustManager;
 import java.io.IOException;
-import java.net.Proxy;
 import java.nio.charset.StandardCharsets;
-import java.util.Stack;
-import java.util.concurrent.TimeUnit;
 
 /**
  * @author wuzi
@@ -33,11 +27,8 @@ public class TabbyChatHandler extends AbstractHandler {
 
     private static final Logger LOG = LoggerFactory.getLogger(TabbyChatHandler.class);
 
-    private final Stack<String> gpt35Stack = new Stack<>();
-
     public EventSource handle(MainPanel mainPanel, MessageComponent component, String question) {
         myProject = mainPanel.getProject();
-        RequestProvider provider = new RequestProvider().create(mainPanel, question);
         try {
             String jsonRequestBody = "{\n" +
                     "  \"messages\": [\n" +
@@ -48,8 +39,11 @@ public class TabbyChatHandler extends AbstractHandler {
                     "  ]\n" +
                     "}";
 
+            String tabbyChatUrl = System.getenv("TABBY_CHAT_URL");
+            LOG.info("TabbyChat: tabbyChatUrl = {}", tabbyChatUrl);
+
             Request request = new Request.Builder()
-                    .url("http://localhost:8080/v1beta/chat/completions")
+                    .url(tabbyChatUrl + "/v1beta/chat/completions")
                     // 这里直接使用提供的 JSON 数据
                     .post(RequestBody.create(jsonRequestBody.getBytes(StandardCharsets.UTF_8),
                             MediaType.parse("application/json")))
@@ -62,12 +56,12 @@ public class TabbyChatHandler extends AbstractHandler {
 
                 @Override
                 public void onOpen(@NotNull EventSource eventSource, @NotNull Response response) {
-                    LOG.info("ChatGPT: conversation open. Url = {}",eventSource.request().url());
+                    LOG.info("TabbyChat: conversation open. Url = {}",eventSource.request().url());
                 }
 
                 @Override
                 public void onClosed(@NotNull EventSource eventSource) {
-                    LOG.info("ChatGPT: conversation close. Url = {}",eventSource.request().url());
+                    LOG.info("TabbyChat: conversation close. Url = {}",eventSource.request().url());
                     if (!handler) {
                         component.setContent("Connection to remote server failed. There are usually several reasons for this:<br />1. Request too frequently, please try again later.<br />2. It may be necessary to set up a proxy to request.");
                     }
@@ -90,7 +84,7 @@ public class TabbyChatHandler extends AbstractHandler {
                         component.setSourceContent(parseResult.getSource());
                         component.setContent(parseResult.getHtml());
                     } catch (Exception e) {
-                        LOG.error("ChatGPT: Parse response error, e={}, message={}", e, e.getMessage());
+                        LOG.error("TabbyChat: Parse response error, e={}, message={}", e, e.getMessage());
                         component.setContent(e.getMessage());
                     } finally {
                         mainPanel.getExecutorService().shutdown();
@@ -101,13 +95,13 @@ public class TabbyChatHandler extends AbstractHandler {
                 public void onFailure(@NotNull EventSource eventSource, @Nullable Throwable t, @Nullable Response response) {
                     if (t != null) {
                         if (t instanceof StreamResetException) {
-                            LOG.info("ChatGPT: Request failure, throwable StreamResetException, cause: {}", t.getMessage());
+                            LOG.info("TabbyChat: Request failure, throwable StreamResetException, cause: {}", t.getMessage());
                             component.setContent("Request failure, cause: " + t.getMessage());
                             mainPanel.aroundRequest(false);
                             t.printStackTrace();
                             return;
                         }
-                        LOG.info("ChatGPT: conversation failure. Url={}, response={}, errorMessage={}",eventSource.request().url(), response, t.getMessage());
+                        LOG.info("TabbyChat: conversation failure. Url={}, response={}, errorMessage={}",eventSource.request().url(), response, t.getMessage());
                         component.setContent("Response failure, cause: " + t.getMessage() + ", please try again. <br><br> Tips: if proxy is enabled, please check if the proxy server is working.");
                         mainPanel.aroundRequest(false);
                         t.printStackTrace();
@@ -118,12 +112,12 @@ public class TabbyChatHandler extends AbstractHandler {
                                 responseString = response.body().string();
                             } catch (IOException e) {
                                 mainPanel.aroundRequest(false);
-                                LOG.error("ChatGPT: parse response error, cause: {}", e.getMessage());
+                                LOG.error("TabbyChat: parse response error, cause: {}", e.getMessage());
                                 component.setContent("Response failure, cause: " + e.getMessage());
                                 throw new RuntimeException(e);
                             }
                         }
-                        LOG.info("ChatGPT: conversation failure. Url={}, response={}",eventSource.request().url(), response);
+                        LOG.info("TabbyChat: conversation failure. Url={}, response={}",eventSource.request().url(), response);
                         component.setContent("Response failure, please try again. Error message: " + responseString);
                     }
                     mainPanel.aroundRequest(false);
@@ -134,7 +128,7 @@ public class TabbyChatHandler extends AbstractHandler {
             EventSource.Factory factory = EventSources.createFactory(httpClient);
             return factory.newEventSource(request, listener);
         } catch (Exception e) {
-            LOG.error("ChatGPT handle Exception, error: {}", e.getMessage());
+            LOG.error("TabbyChat handle Exception, error: {}", e.getMessage());
         } finally {
             mainPanel.getExecutorService().shutdown();
         }
